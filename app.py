eacf94706f7e826ffdb0788b6ef4ba302e14c71e
RobNewBYO <rob.new@byojet.com>
2021-02-05 10:43:00 +1000

    Collapse sections and maintain results

--- app.py
+++ app.py
@@ -5,14 +5,12 @@ if platform.system()=='Windows':
     os.chdir('C:/Users/rob.new/Downloads/Applications/WPy64-3870/projects/nettingEngine_local/')
 
 import pandas as pd
-# import json
-# import pyDMNrules
+import pyDMNrules
 import netEnginefunctions as func
-# import datetime
-# import requests
 import streamlit as st
 st.set_page_config(layout = 'wide')
 import streamlit.components.v1 as components
+import SessionState
 from streamlit.report_thread import get_report_ctx
 ctx = get_report_ctx()
 session_id = ctx.session_id
@@ -25,24 +23,72 @@ session_id = ctx.session_id
 def get_pccs(session_id = session_id):
     return[]
 
+def get_results(PccList, Origin, Destination, Departure, TripLength, Currency, PaxType, session_id = session_id):
+    results = None
+    for x in PccList:
+        print(x)
+        if x['Token'] == 'Auth failure':
+            print('Ignored ' + x['PCC'] + ' entry because of Auth Failure')
+        else:
+            tempresults = func.getresponse(x['PCC'], x['Token'], Origin, Destination, Departure, TripLength, Currency, PaxType)
+            if len(tempresults)>0:
+                print('Successfully retrieved results from ' + x['PCC'])
+            if results is None:
+                results = tempresults
+            else:
+                results = results.append(tempresults, ignore_index = True)
+    
+    return results
+
+
+st.sidebar.markdown('### Sections')
 info = st.sidebar.checkbox('Read me')
+config = st.sidebar.checkbox('Configurations', value = True)
+r = st.sidebar.empty()
+request = r.checkbox('Search Request')
 
-auth_entry = st.sidebar.text_input('Auth Entry', '')
-st.sidebar.markdown( 
-    """<a href="https://ppsdns-my.sharepoint.com/:x:/g/personal/rob_new_byojet_com/EUnh714GBppKtvjt5XiOWsYBGkEX86wkIPDaKkmeO6dYlQ?e=eavipI" target="_blank">Auth Credentials</a>""", unsafe_allow_html=True,
-    )
+if config:
+    st.sidebar.markdown('### Configuration')
+    auth_entry = st.sidebar.text_input('Auth Entry', '')
+    st.sidebar.markdown( 
+        """<a href="https://ppsdns-my.sharepoint.com/:x:/g/personal/rob_new_byojet_com/EUnh714GBppKtvjt5XiOWsYBGkEX86wkIPDaKkmeO6dYlQ?e=eavipI" target="_blank">Auth Credentials</a>""", unsafe_allow_html=True,
+        )
+    
+    if st.sidebar.button('auth'):
+        PCC = auth_entry.split("|")[0]
+        EPR = auth_entry.split("|")[1]
+        pwd = auth_entry.split("|")[2]
+        temp_tkn = func.authme(PCC, EPR, pwd)
+        get_pccs().append({'PCC':PCC, 'Token': temp_tkn})
+        # PCC_default = ''
+        # EPR_default = ''
+        # pwd_default = ''    
+    
+    # uploaded_file = st.sidebar.file_uploader("Add your commercials file")
+    # if uploaded_file is not None:
+    #     dmnRules = pyDMNrules.DMN()
+    #     status = dmnRules.load(uploaded_file)
+    #     if 'errors' in status:
+    #         print('nettingEngineDecisions.xlsx has errors', status['errors'])
+    #         # sys.exit(0)
+    #     else:
+    #         print('nettingEngineDecisions.xlsx loaded')
 
-if st.sidebar.button('auth'):
-    PCC = auth_entry.split("|")[0]
-    EPR = auth_entry.split("|")[1]
-    pwd = auth_entry.split("|")[2]
-    temp_tkn = func.authme(PCC, EPR, pwd)
-    get_pccs().append({'PCC':PCC, 'Token': temp_tkn})
-    # PCC_default = ''
-    # EPR_default = ''
-    # pwd_default = ''
+if len(get_pccs())>0:
+    request = r.checkbox('Search Request', value = True)
 
+## Request compiler
+# if request:
+st.sidebar.markdown('### Search Parameters')
+Origin = st.sidebar.text_input('3-digit origin airport','BNE')
+Destination = st.sidebar.text_input('3-digit destination airport','SYD')
+Departure = st.sidebar.text_input('date in %Y-%m-%d format','2021-06-06')
+TripLength = st.sidebar.number_input('Trip Length', min_value = 1, max_value = 365, value = 7)
+Currency = st.sidebar.text_input('currency', 'AUD')
+PaxType = st.sidebar.text_input('pax type', 'ITX')
+run = st.sidebar.button('run')
 
+## Body
 if info:
     help_text = func.help_text
 else:
@@ -50,40 +96,21 @@ else:
     
 st.markdown(help_text)
 
-st.markdown('## PCC Authorization')
-pccs = pd.DataFrame(get_pccs())
-st.write(pccs)
+if config:
+    st.markdown('## PCC Authorization')
+    pccs = pd.DataFrame(get_pccs())
+    st.write(pccs)
+
+session_state = SessionState.get(results = pd.DataFrame())
+if run:
+    results = get_results(get_pccs(),  Origin, Destination, Departure, TripLength, Currency, PaxType)
+    session_state.results = results
+
+t = func.pivot_ui(session_state.results, rows = ['carrier'], cols = ['pcc'], vals = ['totalamount'])
+st.markdown('## Results')
+with open(t.src) as t:
+    components.html(t.read(), width = 1500, height = 1000, scrolling = True)
 
 
-## Request compiler
-Origin = st.sidebar.text_input('3-digit origin airport','BNE')
-Destination = st.sidebar.text_input('3-digit destination airport','SYD')
-Departure = st.sidebar.text_input('date in %Y-%m-%d format','2021-06-06')
-TripLength = st.sidebar.number_input('Trip Length', min_value = 1, max_value = 365, value = 7)
-Currency = st.sidebar.text_input('currency', 'AUD')
-PaxType = st.sidebar.text_input('pax type', 'ITX')
-if st.sidebar.button('run'):
-    results = None
-    for x in get_pccs():
-        if x['Token'] == 'Auth failure':
-            print('Ignored ' + x['PCC'] + ' entry because of Auth Failure')
-        else:
-            tempresults = func.getresponse(x['PCC'], x['Token'], Origin, Destination, Departure, TripLength, Currency, PaxType)
-            if results is None:
-                results = tempresults
-            else:
-                print('Successfully retrieved results from ' + x['PCC'])
-                results = results.append(tempresults, ignore_index = True)
-    
-    t = func.pivot_ui(results, rows = ['carrier'], cols = ['pcc'], vals = ['totalamount'])
-    st.markdown('## Results')
-    with open(t.src) as t:
-        components.html(t.read(), width = 1500, height = 1000, scrolling = True)
-
-
-## To do
-## Add more parameters
-## Netting down
-## save results
 
 
